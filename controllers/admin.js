7c77f3239cd086e101e65f055072f354e2b2c248
BryanCox <bryanc71@yahoo.com>
2018-03-15 10:57:30 -0700

    admin delete functional

---
+++ controllers/admin.js
@@ -0,0 +1,26 @@
+const knex = require("../db/knex.js");
+
+module.exports = {
+
+  home_page: function(req, res){
+    knex('boards')
+    .then((results)=>{
+      res.render('admin', {theBoards:results})
+    })
+  },
+
+  delete_board: function(req, res) {
+    knex('boards')
+      .where('board_id', req.params.id)
+        .del()
+        .then(()=>{
+          res.redirect('/admin')
+        })
+        .catch((error)=>{
+          console.log(error);
+          res.sendStatus(500);
+        })
+  }
+
+
+}
--- db/migrations/20180312104619_04_cards.js
+++ db/migrations/20180312104619_04_cards.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('cards', function(table){
     table.increments('card_id');
       table.string('card_name')
-      table.integer('parent_column_id')
+      table.integer('parent_column_id').references('column_id').inTable('columns')
       table.text('content')
       table.timestamp('updated_at').defaultTo(knex.fn.now());
       table.timestamp('ucreated_at').defaultTo(knex.fn.now());
